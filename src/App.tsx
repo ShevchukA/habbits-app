a75c66c50cbcc5b4bcedbe91cbfc99ae6143c02d
ShevchukA <antoshka.vbg@mail.ru>
2025-01-12 18:38:21 +0000

    feat: vite test setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+
+import { ChangeEvent, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [habit, setHabit] = useState('');
+  const [habits, setHabits] = useState<string[]>([]);
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setHabit(e.target.value);
+  };
+
+  const handleAddHabit = () => {
+    const newHabits = [...habits, habit];
+    setHabits(newHabits);
+    setHabit('');
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <ul>
+        {habits?.map((habit, i) => (
+          <li key={i}>{habit}</li>
+        ))}
+      </ul>
+
+      <input type='text' value={habit} onChange={handleInput}></input>
+      <button onClick={handleAddHabit}>Add habit</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
